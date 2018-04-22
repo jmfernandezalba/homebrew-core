8a1e236fd69865d53073412fd11085180d11a9ba
Jose M. Fernandez-Alba <josemaria@starzplayarabia.com>
2018-04-22 12:31:13 +0200

    Use just git head as version

--- Formula/zq.rb
+++ Formula/zq.rb
@@ -1,16 +1,11 @@
 class Zq < Formula
   desc "Script to query all zuul endpoints properties"
   homepage "https://github.com/jmfernandezalba/zq"
-  url "https://raw.githubusercontent.com/jmfernandezalba/zq/master/zq"
-  version "1.0"
-  sha256 ""
+  head "https://raw.githubusercontent.com/jmfernandezalba/zq/master/zq"
+  sha256 "2739f1dfb38d1828ddac243607837c9700635d596e2bbd06c97c997be31a8ece"
   depends_on "jq"
 
   def install
     bin.install "zq"
-  end
-
-  test do
-    system "true"
   end
 end
